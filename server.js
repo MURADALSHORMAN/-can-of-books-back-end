a16f1b19e5133c53a0999ca11a735b8cf3415133
murad alshorman <muradalshorman@gmail.com>
2021-05-18 20:44:14 +0300

    lab13 back end

--- server.js
+++ server.js
@@ -74,11 +74,47 @@ function muradSeed() {
 
 app.get("/", homePage);
 
+
+
+// new route for deleting the book by its ID
+app.delete('/books/:index', deletebookForOwner);
+
+function deletebookForOwner(req, res) {
+
+  const index = Number(req.params.index);
+ 
+  const { email } = req.query;
+
+  userModel.find({ email: email }, (err, userData) => {
+      const newCatsArr =  userData[0].books.filter((book, idx) => {
+          return idx !== index
+      });
+      userData[0].books = newCatsArr;
+      userData[0].save();
+      res.send(' book deleted')
+  });
+}
+
+////////////////////////////////////////////////////////////
+// new route for creating the new book data
+app.post('/books', createbook);
+////////////////////////////////////////////////////////////
+function createbook(req, res) {
+   const { name, email, description } = req.body;
+    myOwnerModel.find({ email: email }, (error, ownerData) => {
+     
+          userData[0].books.push({
+          name: name,
+          description: description
+      })
+      userData[0].save();
+       res.send(userData[0].books);
+  });
+}
+/////////////////////////////////////////////////////////////////
 function homePage(req, res) {
   res.send("This is my Home Page");
 }
-
-
 // Endpoint for retrieving books data
 app.get("/books", booksPage);
 
@@ -86,10 +122,11 @@ function booksPage(req, res) {
   const { email } = req.query;
   userModel.find({ email: email }, "books", function (error, userData) {
     if (error) res.send("Something went wrong!");
-    console.log(userData);
-    res.send(userData);
+    console.log(userData[0].books);
+    res.send(userData[0].books);
   });
 }
+
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
